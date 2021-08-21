b75d0503ac87c2da144662472b451d99bd56ab2e
Lakmal Deshapriya <lakmal.deshapriya@xiatadigitallabs.com>
2021-08-21 11:07:22 +0530

    get name

--- run.java
+++ run.java
@@ -2,5 +2,10 @@ public class Run{
     public static void main(String[] args) {
         System.out.println("hkjzdcoaslkdczw");
         System.out.println("Dilusha");
+        System.out.println(viewUI());
+    }
+
+    public static String viewUI(){
+        return  "Dilusha Malawige git claass";
     }
 }
